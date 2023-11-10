5e3b9e8de6ca7dfb90c1ea6223059bdd7349f729
unknown <juanchicanestrari@gmail.com>
2023-11-10 16:11:06 -0300

    FEAT (Miembros & AgregarMiembro) Conexion con el backend PROXIMO BORRAR

--- src/Componentes/Miembros.jsx
+++ src/Componentes/Miembros.jsx
@@ -9,15 +9,9 @@ export const Miembros = () => {
 
     const [miembros, setMiembros] = useState([]);
 
-    // const eliminar = (id) =>{
-    //   const nuevos = miembros.filter(miembro=>miembro.id !== id);
-    //   setmiembros(nuevos);
-    // }
-
     useEffect(() => {
-      // Realizar una solicitud al servidor para obtener los miembros cuando el componente se monta
       obtenerMiembros();
-    }, []); // El segundo parámetro [] asegura que esta solicitud se realice solo una vez al montar el componente
+    }, []); 
   
     const obtenerMiembros = async () => {
       try {
@@ -34,7 +28,6 @@ export const Miembros = () => {
       try {
         await axios.delete("http://localhost:8080/miembro/${id}");
         console.log('Miembro eliminado correctamente');
-        // Actualiza la lista de miembros después de eliminar uno
         obtenerMiembros();
       } catch (error) {
         console.error('Error al eliminar miembro:', error);
