17116840f5e98d77b1a843a1eae5772cd536a161
KanyeIsTheGoat <marcoojulianoo@gmail.com>
2023-11-13 20:31:22 -0300

    docus casi ready

--- src/Componentes/documento/Documentos.jsx
+++ src/Componentes/documento/Documentos.jsx
@@ -10,24 +10,54 @@ export const Documentos = () => {
     otros: false,
   });
 
+  useEffect(() => {
+    obtenerDocumentos();
+  }, []);
   /*
     localhost:8080/documento/viaje/{idViaje} -> esta llamada devuelve todos los id de documentos, nombre y tipo de un idViaje
     luego si lo queres ver podes llamar a -> localhost:8080/documento/{id} 
   */
+  const obtenerDocumentos = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/documento/viaje/1"
+      );
+      setDocumentosGuardados(response.data);
+      console.log("Documentos obtenidos correctamente:", response.data);
+    } catch (error) {
+      console.error("Error al obtener documentos:", error);
+    }
+  };
 
-  useEffect(() => {
-    const obtenerDocumentos = async () => {
-      try {
-        const response = await axios.get("http://localhost:8080/documento/viaje/1");
-        setDocumentosGuardados(response.data);
-        console.log('Documentos obtenidos correctamente:', response.data);
-      } catch (error) {
-        console.error("Error al obtener documentos:", error);
-      }
-    };
+  const eliminarDocumentos = async (idDocumento) => {
+    await axios.delete("http://localhost:8080/documento/"+idDocumento , {responseType: "arraybuffer"})
 
-    obtenerDocumentos();
-  }, []);
+    setDocumentosGuardados((prevDocumentos) =>
+      prevDocumentos.filter((doc) => doc.id !== idDocumento)
+    );
+  }
+
+  const descargarDocumento = async (idDocumento,nombreDocumento) => { 
+    const response= await axios.get("http://localhost:8080/documento/"+idDocumento+"/descargar")
+    console.log(response.data)
+
+    const blob = new Blob([response.data], {type: "application/pdf"})
+
+    const url = window.URL.createObjectURL(blob)
+
+    const a = document.createElement('a');
+    a.href= url;
+    a.download= nombreDocumento;
+    document.body.appendChild(a);
+    a.click();
+
+    window.URL.revokeObjectURL(url)
+    document.body.removeChild(a)
+    
+  }
+
+
+
 
   const toggleVisibilidadLista = (tipo) => {
     setVisibilidadListas((prevVisibilidadListas) => ({
@@ -38,16 +68,21 @@ export const Documentos = () => {
 
   const ListaDesplegable = ({ tipo, documentos }) => (
     <div>
-      <h2 onClick={() => toggleVisibilidadLista(tipo)} style={{ cursor: "pointer" }}>
+      <h2
+        onClick={() => toggleVisibilidadLista(tipo)}
+        style={{ cursor: "pointer" }}
+      >
         {tipo} {visibilidadListas[tipo] ? "▼" : "►"}
       </h2>
       {visibilidadListas[tipo] && (
         <ul>
           {documentos
-            .filter((doc) => doc.tipo === tipo) 
+            .filter((doc) => doc.tipo === tipo)
             .map((doc, index) => (
               <li key={index}>
                 <strong>Título:</strong> {doc.nombreDocumento}
+                <button onClick={() => descargarDocumento(doc.id,doc.nombreDocumento)}>Descargar</button>
+                <button onClick={() => eliminarDocumentos(doc.id)}>Eliminar</button>
               </li>
             ))}
         </ul>
