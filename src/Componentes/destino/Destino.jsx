9703a58c418d7a85dcc6d49bea54e15064e02024
KanyeIsTheGoat <marcoojulianoo@gmail.com>
2023-11-22 00:52:12 -0300

    asdasdas

--- src/Componentes/destino/Destino.jsx
+++ src/Componentes/destino/Destino.jsx
@@ -1,78 +1,92 @@
-import '../../Styles/destino.css';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useState } from "react"
-import axios from "axios";
-
-
+import axios from 'axios';
+import '../../Styles/destino.css';
 
 export const Destino = () => {
+  const [destino, setDestino] = useState('');
+  const [fechaInicio, setFechaInicio] = useState('');
+  const [fechaFin, setFechaFin] = useState('');
 
-    const [destino, setDestino] = useState('');
-    const [fechaIncio, setFechaInicio] = useState('');
-    const [fechaFin, setFechaFin] = useState('');
+  const [viaje, setViaje] = useState([]);
 
-    const [viaje, setViaje] = useState([]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      const nuevoViaje = { destino, fechaIncio, fechaFin };
-      setViaje([...viaje, nuevoViaje]);
-      setDestino('');
-      setFechaInicio('');
-      setFechaFin('');
-     
-      if (!destino || !fechaIncio || !fechaFin) {
-          alert('Por favor, completa todos los campos');
-          return;
-      } 
-      try {
-                   
-          await axios.post('http://localhost:8080/destino', {destino, fechaIncio, fechaFin, viajeId:sessionStorage.getItem('viajeId')});
-          console.log('Datos enviados correctamente');
-      } catch (error) {
-          console.error('Error al enviar datos:', error);
-      }
+    // Validar campos
+    if (!destino || !fechaInicio || !fechaFin) {
+      alert('Por favor, completa todos los campos');
+      return;
+    }
 
-    };
+    const nuevoViaje = { destino, fechaInicio, fechaFin };
+    setViaje([...viaje, nuevoViaje]);
 
+    try {
+      await axios.post('http://localhost:8080/destino', {
+        destino,
+        fechaInicio,
+        fechaFin,
+        viajeId: sessionStorage.getItem('viajeId'),
+      });
+      console.log('Datos enviados correctamente');
+    } catch (error) {
+      console.error('Error al enviar datos:', error);
+    }
+
+    // Limpiar campos después del envío exitoso
+    setDestino('');
+    setFechaInicio('');
+    setFechaFin('');
+  };
 
   return (
-    
     <div>
+      <header className="header-destinos">
+        <Link to="/Home" className="go-back">
+          <i className='bx bx-chevron-left'></i>
+        </Link>
+        <Link to="/Home" className="miembros">
+          Destinos
+        </Link>
+        <Link to="/Home" className="icon"></Link>
+      </header>
 
+      <section className="lista-miembros2" id="section-a">
+        <div className="form1-destinos">
+          <form onSubmit={handleSubmit}>
+            <p className="p-style1">Destino</p>
+            <input
+              type="text"
+              placeholder=""
+              value={destino}
+              onChange={(e) => setDestino(e.target.value)}
+            />
 
-        <header className="header-destinos">
-      <a href="/Home" className="go-back"><i className='bx bx-chevron-left'></i></a>
-      <a href="/Home" className="miembros">Destinos</a>
-      <a href="/Home" className="icon"></a>
-    </header>
-      
-    <section className="lista-miembros2" id="section-a">
-    <div className="form1-destinos">
-      <form onSubmit={handleSubmit}>
+            <p className="p-style">Fecha de Inicio</p>
+            <input
+              type="date"
+              placeholder="yyyy-mm-dd"
+              value={fechaInicio}
+              onChange={(e) => setFechaInicio(e.target.value)}
+            />
 
-        <p className="p-style1">Destino</p>
+            <p className="p-style">Fecha de Finalizacion</p>
+            <input
+              type="date"
+              placeholder="yyyy-mm-dd"
+              value={fechaFin}
+              onChange={(e) => setFechaFin(e.target.value)}
+            />
 
-            <input type="text" placeholder="" value={destino} onChange={(e) => setDestino(e.target.value)}/>
-              <p className="p-style">Fecha de Inicio</p>
-            <input type="date" placeholder="yyyy-mm-dd"  value={fechaIncio} onChange={(e) => setFechaInicio(e.target.value)}/>
-              <p className="p-style">Fecha de Finalizacion</p>
-            <input type="date" placeholder="yyyy-mm-dd"  value={fechaFin} onChange={(e) => setFechaFin(e.target.value)}/>
             <br></br>
 
-            <button type="submit" className='add-destino'>Confirmar destino</button>
-
-        </form>
+            <button type="submit" className='add-destino'>
+              Confirmar destino
+            </button>
+          </form>
         </div>
-
-        
-
-</section>
-
-
+      </section>
     </div>
-
-
-  
-  )
-}
+  );
+};
--- src/Componentes/miembros/Miembros.jsx
+++ src/Componentes/miembros/Miembros.jsx
@@ -1,76 +1,66 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import '../../Styles/miembros.css';
 
-
 export const Miembros = () => {
+  const [miembros, setMiembros] = useState([]);
 
+  useEffect(() => {
+    obtenerMiembros();
+  }, []); 
 
+  const obtenerMiembros = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/miembro');
+      setMiembros(response.data);
+      console.log('Miembros obtenidos correctamente:', response.data);
+    } catch (error) {
+      console.error('Error al obtener miembros:', error);
+    }
+  };
 
-    const [miembros, setMiembros] = useState([]);
-
-    useEffect(() => {
+  const handleEliminarMiembro = async (miembroId) => {
+    console.log(miembroId);
+    try {
+      await axios.delete(`http://localhost:8080/miembro/${miembroId}`);
+      console.log('Miembro eliminado correctamente');
+      // Actualiza el estado después de la eliminación
       obtenerMiembros();
-    }, []); 
-  
-    const obtenerMiembros = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/miembro');
-        setMiembros(response.data);
-        console.log(miembros);
-        console.log('Miembros obtenidos correctamente:', response.data);
-      } catch (error) {
-        console.error('Error al obtener miembros:', error);
-      }
-    };
+    } catch (error) {
+      console.error('Error al eliminar miembro:', error);
+    }
+  };
 
-    const handleEliminarMiembro = async (id) => {
-      console.log(id)
-      try {
-        await axios.delete("http://localhost:8080/miembro/${id}");
-        console.log('Miembro eliminado correctamente');
-        obtenerMiembros();
-      } catch (error) {
-        console.error('Error al eliminar miembro:', error);
-      }
-    };
-
-   return (
+  return (
     <div>
-    <header className="header-miembros">
-      <a href="/Home" className="go-back"><i className='bx bx-chevron-left'></i></a>
-      <a href="/Home" className="miembros">Miembros</a>
-      <a href="/Home" className="icon"></a>
-    </header>
+      <header className="header-miembros">
+        <a href="/Home" className="go-back"><i className='bx bx-chevron-left'></i></a>
+        <a href="/Home" className="miembros">Miembros</a>
+        <a href="/Home" className="icon"></a>
+      </header>
 
-    <section className="lista-miembros" id="section-a">
-      <h1 className="title-miembros">Lista de miembros</h1>
-      {miembros.map((miembro) => (
-          <div className="container-miembros" key={miembro.id}>
+      <section className="lista-miembros" miembroId="section-a">
+        <h1 className="title-miembros">Lista de miembros</h1>
+        {miembros.map((miembro) => (
+          <div className="container-miembros" key={miembro.miembroId}>
             <p className="user-icon-miembros"><i className='bx bx-user-circle'></i></p>
             <p className="member-miembros">{miembro.nombre}</p>
-            <button className="eliminar-miembro" onClick={() => handleEliminarMiembro(miembro.id)}>Eliminar</button>
+            <button className="eliminar-miembro" onClick={() => handleEliminarMiembro(miembro.miembroId)}>Eliminar</button>
           </div>
         ))}
 
-      
+        <Link to="/AgregarMiembro" className="link-miembros">
+          <div className="add-miembro">
+            <p>Añadir Miembro</p>
+          </div>
+        </Link>
 
-    <Link to="/AgregarMiembro" className="link-miembros">
-    <div className="add-miembro">
+        <Link to="/Miembros/CrearEncuesta">
+          <button className="add-miembro">Crear Encuesta</button>
+        </Link>
 
-        <p>Añadir Miembro</p>
-
+      </section>
     </div>
-    </Link>
-
-    
-    <Link to="/Miembros/CrearEncuesta">
-            <button className="add-miembro">Crear Encuesta</button>
-    </Link>
-
-    </section>
-
-  </div>
-  )
-}
+  );
+};
